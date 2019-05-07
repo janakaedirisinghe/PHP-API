53f18222842d511b8ac2926ff38e71227e71136a
janaka531 <janakapradeepedirisinghe@gmail.com>
2019-05-08 00:52:36 +0530

    get api

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,13 +82,7 @@
                     Laravel
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <a href="about">About</a>
             </div>
         </div>
     </body>
