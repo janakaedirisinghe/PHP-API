d553fa90b42802dd2b61527d63d3378ec0c609de
janaka531 <janakapradeepedirisinghe@gmail.com>
2019-05-08 02:37:50 +0530

    get and post API

--- app/Http/Controllers/Youtube.php
+++ app/Http/Controllers/Youtube.php
@@ -19,17 +19,18 @@ class Youtube extends Controller
      * @param Request $request
      */
     public function insert(Request $request){
-       echo ($request->input('id'));
-        echo ($request->input('OrderNumber'));
-        echo ($request->input('user_id'));
+
 
         $order = new Order();
         $order->id = $request->input('id');
         $order->OrderNumber = $request->input('OrderNumber');
         $order->user_id = $request->input('user_id');
 
-        $order->save();
+        $result = $order->save();
 
+        if ($result){
+            echo 'record added!';
+        }
 
 
 
