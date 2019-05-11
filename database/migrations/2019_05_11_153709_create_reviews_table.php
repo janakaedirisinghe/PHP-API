986d103c785b42e298881cfac5d679cbd74b785f
janaka531 <janakapradeepedirisinghe@gmail.com>
2019-05-11 21:39:22 +0530

    api resources

--- database/migrations/2019_05_11_153709_create_reviews_table.php
+++ database/migrations/2019_05_11_153709_create_reviews_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTasksTable extends Migration
+class CreateReviewsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,13 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
+        Schema::create('reviews', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('description');
-            $table->boolean('isCompleted');
+            $table->integer('task_id')->unsigned()->index();
+            $table->foreign('task_id')->references('id')->on('tasks')->onDelete('cascade');
+
+            $table->text('review');
+            $table->integer('star');
             $table->timestamps();
         });
     }
@@ -29,6 +31,6 @@ class CreateTasksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('reviews');
     }
 }
